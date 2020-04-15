b19081330990bf2842061eb7164dfa559f242319
FranciG <beodo0@gmail.com>
2020-04-15 14:50:31 +0200

    Weapon development

---
+++ Assets/Scripts/MonoBehaviours/Ammo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class Ammo : MonoBehaviour
+{
+// amount of damage
+    public int damageInflicted;
+// Called when another object enters the Trigger Collider
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+// check if we hit the BoxCollider2D inside the enemy
+if (collision is BoxCollider2D)
+        {
+// Retrieve the Enemy script component of the gameObject from the collision
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+// Start the Coroutine to damage the Enemy.
+//Parameters: the amount of damage and damage interval (time to wait between inflicting damage)
+//Interval = 0 will inflict damage a single time.
+//damageInflicted is set in Unity editor
+            StartCoroutine(enemy.DamageCharacter(damageInflicted, 0.0f));
+// Because the ammo has struck the Enemy, set the gameObject of the AmmoObject to be inactive.
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/Weapon.cs
@@ -0,0 +1,32 @@
+﻿// import System.Collections.Generic to use the List data structure
+//A variable of type: List is used to represent the object pool
+using System.Collections.Generic;
+using UnityEngine;
+// Weapon inherits from MonoBehaviour and thus can be attached to a GameObject
+public class Weapon : MonoBehaviour
+{
+// The property ammoPrefab is set via the Unity Editor, used to instantiate copies of the AmmoObject
+//that will be added to a pool of objects in the Awake() method.
+    public GameObject ammoPrefab;
+// property ammoPool of type: List is used to represent the object pool
+    static List<GameObject> ammoPool;
+// poolSize allows us to set the number of objects to be pre-instantiated in the object pool
+
+    public int poolSize;
+// The code to create the Object Pool and pre-initialize the AmmoObjects will be contained in the Awake() method 
+    void Awake()
+    {
+// Check to see if the ammoPool object pool has been initialized already. If it has not been initialized, create a new ammoPool of type: List to hold GameObjects.
+        if (ammoPool == null)
+        {
+            ammoPool = new List<GameObject>();
+        }
+// Create a loop using poolSize as the upper limit
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject ammoObject = Instantiate(ammoPrefab);
+            ammoObject.SetActive(false);
+            ammoPool.Add(ammoObject);
+        }
+    }
+}
